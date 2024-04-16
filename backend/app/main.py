b7c60bc7769cf6144d0e6c0dcb90ff31ed994179
FrancescoPiscopello <893994@stud.unive.it>
2024-04-16 21:40:06 +0200

    Still not fixed, minor changes to get_courses function

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,19 +7,23 @@ as the backend for the project.
 
 from fastapi import FastAPI
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from datetime import datetime
+import pandas as pd
+from fastapi import Query
 from .mymodules.df_creating import df_creating
 import json
 
 app = FastAPI()
 
 
+final_urls_dataframe = df_creating()
 
 @app.get('/csv_show')
-def read_and_return_df():
+def read_and_return_csv():
     """
     """
-    df = df_creating()
-    return df
+    return final_urls_dataframe
 
 @app.get('/')
 def read_root():
@@ -34,23 +38,20 @@ def read_root():
 @app.get("/query/{insegnamento_name}/{location_str}")
 def get_courses_taught_by_person(insegnamento_name, location_str):
     """
-
     """
-
-    # Convert to title case for consistency
-
-    insegnamento_name = insegnamento_name.title()  
     
-    # Convert the comma-separated location string into a list
+    insegnamento_name = insegnamento_name.title()  # Convert to title case for consistency
+    # Filter the DataFrame to rows where the person's name appears in the 'DOCENTI' column
+    
+    # Convert the comma-separated locations string into a list
     luoghi_list = location_str.split(",") if location_str else []
-    
-    filtered_df = df_creating()
 
-    
+    filtered_df = final_urls_dataframe
+
     if luoghi_list:
-        filtered_df = filtered_df[filtered_df['LUOGO'].isin(luoghi_list)]
+        filtered_df = filtered_df[filtered_df['LOCATION_NAME'].isin(luoghi_list)]
             
-    insegnamenti = filtered_df[filtered_df['NOME_INSEGNAMENTO'].str.contains(insegnamento_name, case=False, na=False)]
+    insegnamenti = filtered_df[filtered_df['TEACHING'].str.contains(insegnamento_name, case=False, na=False)]
     
     insegnamenti_dict = insegnamenti.to_dict(orient='index')
 
