8dbf0f51ed6e54e664b4844a4f617f8c025ee337
martagriselin <martagriselin@gmail.com>
2024-06-10 21:34:36 +0200

    initialising testing environment

---
+++ backend/tests/test_df_creating.py
@@ -0,0 +1,12 @@
+from app.mymodules.df_creating import df_creating
+
+"""
+Execute this test by running on the terminal (from the app/) the command:
+pytest --cov=app --cov-report=html tests/
+ """
+
+
+#def test_dfcreating():
+
+
+# now run pytest test_df_creating in the terminal 
