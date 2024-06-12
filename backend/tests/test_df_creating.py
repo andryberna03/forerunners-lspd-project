cedc8df615a13cdfb3c586f1c791183564d87f4a
tonigggg <antoniognoni99@gmail.com>
2024-06-12 23:09:30 +0200

    tests on main.py and df_creating.py

--- backend/tests/test_df_creating.py
+++ backend/tests/test_df_creating.py
@@ -1,12 +1,81 @@
-from app.mymodules.df_creating import df_creating
 
+import os
+import datetime
+from unittest.mock import patch
+from app.mymodules.df_creating import df_creating
+import pandas as pd
+import pytest
 """
 Execute this test by running on the terminal (from the app/) the command:
 pytest --cov=app --cov-report=html tests/
  """
+file_path = 'app/final.csv'
 
 
-#def test_dfcreating():
+
+def test_file_is_csv():
+    expected_header = [
+    "AF_ID", "TEACHING", "CYCLE", "PARTITION", "SITE", "CREDITS", "DEGREE_TYPE",
+    "LECTURE_DAY", "LECTURE_START", "LECTURE_END", "LECTURER_NAME", "CLASSROOM_NAME",
+    "LOCATION_NAME", "ADDRESS", "DOCENTE_ID", "URL_DOCENTE", "URLS_INSEGNAMENTO",
+    "START_ISO8601", "END_ISO8601"]
+
+    # Check if the file exists
+    assert os.path.exists(file_path), f"File {file_path} does not exist."
+
+    # Check if the file is a CSV by attempting to read it with pandas
+    try:
+        df = pd.read_csv(file_path)
+    except Exception as e:
+        pytest.fail(f"File {file_path} is not a valid CSV file. Error: {e}")
+
+    # Check if the header row matches the expected header
+    assert list(df.columns) == expected_header, \
+        f"CSV header does not match expected header. Found: {list(df.columns)}, Expected: {expected_header}"
+
+
+
+def test_file_created_within_24_hours():
+    # Verificare che il file esiste
+    assert os.path.exists(file_path), "Il file final.csv non esiste."
+
+    # Ottenere il tempo di creazione del file
+    file_stat = os.stat(file_path)
+    creation_time = file_stat.st_ctime
+
+    # Calcolare l'ora corrente e l'intervallo di 24 ore fa
+    now = datetime.datetime.now().timestamp()
+    twenty_four_hours_ago = now - 24 * 60 * 60
+
+    # Verificare che il file sia stato creato meno di 24 ore fa
+    assert creation_time > twenty_four_hours_ago, "Il file final.csv è stato creato più di 24 ore fa."
+
+
+# # Test 2: Test for non-existing file
+
+# # Test 3: Test for outdated file
+
+
+# # Test 4: Test for API requests
+# @patch('app.mymodules.df_creating.requests.get')
+# def test_api_requests(mock_get):
+#     # Mock API responses
+#     mock_responses = {
+#         'degrees': {'col1': [1, 2, 3], 'col2': ['a', 'b', 'c']},
+#         # Add mock responses for other URLs as needed
+#     }
+#     mock_get.side_effect = lambda url: mock_responses[url.split('/')[-1]]
+#     # Call df_creating and assert it retrieves data from mocked responses
+#     final_df = df_creating()
+#     expected_df = pd.DataFrame(mock_responses['degrees'])
+#     assert final_df.equals(expected_df)
+
+# # Test 5: Test for data consistency and correctness
+# def test_data_consistency_and_correctness():
+#     # Call df_creating
+#     final_df = df_creating()
+#     # Perform assertions to check data consistency and correctness
+#     # For example, check column names, data types, presence of expected data, etc.
 
 import csv
 import os
@@ -32,4 +101,3 @@ def test_csv_file_format():
 # if __name__ == "__main__":
 #   pytest.main()
 
-# now run pytest test_df_creating in the terminal 
--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -1,19 +1,13 @@
 import os
 import sys
 from fastapi.testclient import TestClient
-import os
-import sys
 from unittest.mock import patch
-from fastapi.testclient import TestClient
 from app.main import app
-import pytz
 import datetime
 
+#!!!!!! a cosa serve
 # Add the project root to the sys.path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-# Now you can do the relative import
-from app.main import app
-
+#sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 """
 Execute this test by running on the terminal (from the app/) the command:
@@ -22,12 +16,33 @@ pytest --cov=app --cov-report=html tests/
 
 client = TestClient(app)
 
-
+#ok
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Hello": "World"}
 
+#ok
+def test_read_and_return_df():
+    # Send a GET request to the endpoint
+    response = client.get("/df_show")
+
+    # Assert that the response status code is 200
+    assert response.status_code == 200
+
+    # Get the JSON response data
+    response_data = response.json()
+
+    # Ensure that the response data is a non-empty list
+    assert isinstance(response_data, list)
+    assert len(response_data) > 0
+
+    # Check the structure of the first item in the response data
+    first_item = response_data[0]
+    expected_keys = ['AF_ID', 'TEACHING', 'CYCLE', 'PARTITION', 'SITE', 'CREDITS', 'DEGREE_TYPE', 'LECTURE_DAY', 'LECTURE_START', 'LECTURE_END', 'LECTURER_NAME', 'CLASSROOM_NAME', 'LOCATION_NAME', 'ADDRESS', 'DOCENTE_ID', 'URL_DOCENTE', 'URLS_INSEGNAMENTO', 'START_ISO8601', 'END_ISO8601']
+    assert all(key in first_item for key in expected_keys)
+
+#ok
 @patch('app.main.get_csv_creation_date')  # Mock the get_csv_creation_date function
 def test_csv_creation_date(mock_get_csv_creation_date):
     # Create a datetime object with the desired timezone
@@ -50,26 +65,6 @@ def test_csv_creation_date(mock_get_csv_creation_date):
 
     # Assert that the cookie is set with the correct value
     assert response.cookies.get("creation_date") == expected_date_string
-
-
-def test_read_and_return_df():
-    # Send a GET request to the endpoint
-    response = client.get("/df_show")
-
-    # Assert that the response status code is 200
-    assert response.status_code == 200
-
-    # Get the JSON response data
-    response_data = response.json()
-
-    # Ensure that the response data is a non-empty list
-    assert isinstance(response_data, list)
-    assert len(response_data) > 0
-
-    # Check the structure of the first item in the response data
-    first_item = response_data[0]
-    expected_keys = ['AF_ID', 'TEACHING', 'CYCLE', 'PARTITION', 'SITE', 'CREDITS', 'DEGREE_TYPE', 'LECTURE_DAY', 'LECTURE_START', 'LECTURE_END', 'LECTURER_NAME', 'CLASSROOM_NAME', 'LOCATION_NAME', 'ADDRESS', 'DOCENTE_ID', 'URL_DOCENTE', 'URLS_INSEGNAMENTO', 'START_ISO8601', 'END_ISO8601']
-    assert all(key in first_item for key in expected_keys)
 
 # # testing the query. we will need to change but overall code is correct
 # @patch('app.main.get_courses_taught_by_person')  # Mock the get_courses_taught_by_person function
